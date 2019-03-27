06033291b64a9a8109266716016d5a5d1fb75dd6
caojiali <caojiali1995@gmail.com>
2019-03-27 17:34:56 +0800

    Update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,76 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <div id="mountNode"><h2>This is a home page</h2></div>
 </template>
 
 <script>
+// import dagre from 'dagre';
+import G6 from '@antv/g6';
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  data() {
+    return {
+      data: {
+        nodes: [
+          {id: '1', x: 50, y: 50, size: 20},
+          {id: '2', x: 150, y: 50, size: 20},
+          {id: '3', x: 50, y: 100, size: 20},
+          {id: '4', x: 150, y: 100, size: 20},
+          {id: '5', x: 50, y: 150, size: 20},
+          {id: '6', x: 150, y: 150, size: 20},
+          {id: '7', x: 50, y: 200, size: 20},
+          {id: '8', x: 150, y: 200, size: 20},
+        ],
+        edges: [
+          {source: '1', target: '2', shape: 'line'},
+          {source: '3', target: '4', shape: 'polyline', controlPoints: [{x: 100, y: 70}]},
+          {
+            source: '5', 
+            target: '6',
+            shape: 'quadratic',
+            color: '#722ed1',
+            size: 2,
+            style: {
+              lineDash: [2, 2]
+            },
+            label: 'quadratic',
+            labelCfg: {
+              position: 'center', // 其实默认就是 center，这里写出来便于理解
+              autoRotate: true,
+              style: {
+                lineWidth: 5,
+                stroke: 'white' // 给文本添加白边和白色背景
+              }
+            }
+          },
+          {
+            source: '7', 
+            target: '8', 
+            shape: 'cubic',
+            label: 'quadratic',
+            labelCfg: {
+              autoRotate: true,
+              refY: -10 // refY 默认是顺时针方向向下，所以需要设置负值
+            }
+          }
+        ]
+      }
+    }
+  },
+  mounted() {
+    const data = this.data
+    const graph = new G6.Graph({
+      container: 'mountNode',
+      width: 500,
+      height: 500
+    });
+    graph.data(data);
+    graph.render();
+  },
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
